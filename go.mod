e9953493ff5b924d4e7948f46e40760e093dec6b
EldersJavas <eldersjavas@gmail.com>
2022-07-26 00:34:13 +0800

    Add future

--- go.mod
+++ go.mod
@@ -5,12 +5,14 @@ go 1.18
 require github.com/hajimehoshi/ebiten/v2 v2.3.7
 
 require (
+	github.com/beevik/ntp v0.3.0 // indirect
 	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20220320163800-277f93cfa958 // indirect
 	github.com/gofrs/flock v0.8.1 // indirect
 	github.com/jezek/xgb v1.0.0 // indirect
 	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 // indirect
 	golang.org/x/image v0.0.0-20220321031419-a8550c1d254a // indirect
 	golang.org/x/mobile v0.0.0-20220518205345-8578da9835fd // indirect
+	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
-	golang.org/x/sys v0.0.0-20220408201424-a24fb2fb8a0f // indirect
+	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
 )
--- main.go
+++ main.go
@@ -2,13 +2,45 @@
 
 package main
 
-import "github.com/hajimehoshi/ebiten/v2"
+import (
+	"github.com/EldersJavas/EbiSplit/app"
+	"github.com/beevik/ntp"
+	"github.com/hajimehoshi/ebiten/v2"
+	"log"
+	"os"
+	"time"
+)
 
 func main() {
-	ebiten.SetMaxTPS(30)
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println(r)
+			RunApp()
+		}
+	}()
+	options := ntp.QueryOptions{Timeout: 3 * time.Second}
+	response, err := ntp.QueryWithOptions("0.pool.ntp.org", options)
+	if err != nil {
+		log.Println(err)
+	}
+	if response != nil {
+		time1 := time.Now().Add(response.RTT)
+		log.Println(time1)
+	}
+	RunApp()
+
+}
+func RunApp() {
+	ebiten.SetMaxTPS(10)
 	ebiten.SetRunnableOnUnfocused(true)
 	ebiten.SetScreenTransparent(true)
 	ebiten.SetWindowFloating(true)
 	ebiten.SetWindowTitle("EbiSplit")
+	ebiten.SetWindowSizeLimits(250, 90, 500, 180)
 	ebiten.SetWindowDecorated(true)
+	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
+	err := ebiten.RunGame(app.NewApp(os.Args))
+	if err != nil {
+		log.Fatal(err)
+	}
 }
