dfde61bfbe843b883921f71f4c16cc81098ba5a6
EldersJavas <eldersjavas@gmail.com>
2022-07-26 13:51:20 +0800

    update config

--- config/ebisplitconfig.go
+++ config/ebisplitconfig.go
@@ -1,7 +1,50 @@
 // Created by EldersJavas(EldersJavas&gmail.com)
 
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse and unparse this JSON data, add this code to your project and do:
+//
+//    config, err := UnmarshalConfig(bytes)
+//    bytes, err = config.Marshal()
+
 package config
 
-func main() {
+import "encoding/json"
 
+func UnmarshalConfig(data []byte) (Config, error) {
+	var r Config
+	err := json.Unmarshal(data, &r)
+	return r, err
+}
+
+func (r *Config) Marshal() ([]byte, error) {
+	return json.Marshal(r)
+}
+
+type Config struct {
+	Version    *string     `json:"Version,omitempty"`
+	WindowSize *WindowSize `json:"WindowSize,omitempty"`
+	Background *Background `json:"Background,omitempty"`
+	Foreground *Background `json:"Foreground,omitempty"`
+	Font       *Font       `json:"Font,omitempty"`
+}
+
+type Background struct {
+	Color *Color `json:"Color,omitempty"`
+}
+
+type Color struct {
+	R *int64 `json:"R,omitempty"`
+	G *int64 `json:"G,omitempty"`
+	B *int64 `json:"B,omitempty"`
+	A *int64 `json:"A,omitempty"`
+}
+
+type Font struct {
+	Normal *Background `json:"Normal,omitempty"`
+	Time   *Background `json:"Time,omitempty"`
+}
+
+type WindowSize struct {
+	Width  *int64 `json:"Width,omitempty"`
+	Height *int64 `json:"Height,omitempty"`
 }
--- config/gameconfig.go
+++ config/gameconfig.go
@@ -1,7 +1,36 @@
 // Created by EldersJavas(EldersJavas&gmail.com)
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse and unparse this JSON data, add this code to your project and do:
+//
+//    gameConfig, err := UnmarshalGameConfig(bytes)
+//    bytes, err = gameConfig.Marshal()
 
 package config
 
-func main() {
+import "encoding/json"
 
+func UnmarshalGameConfig(data []byte) (GameConfig, error) {
+	var r GameConfig
+	err := json.Unmarshal(data, &r)
+	return r, err
+}
+
+func (r *GameConfig) Marshal() ([]byte, error) {
+	return json.Marshal(r)
+}
+
+type GameConfig struct {
+	Version    *string      `json:"Version,omitempty"`
+	GameName   *string      `json:"GameName,omitempty"`
+	GameIcon   *string      `json:"GameIcon,omitempty"`
+	RecordList []RecordList `json:"RecordList,omitempty"`
+	TimingOpt  *string      `json:"TimingOpt,omitempty"`
+	TimeFormat *string      `json:"TimeFormat,omitempty"`
+	Site       *string      `json:"Site,omitempty"`
+}
+
+type RecordList struct {
+	Player *string `json:"Player,omitempty"`
+	Time   *string `json:"Time,omitempty"`
+	Note   *string `json:"note,omitempty"`
 }
